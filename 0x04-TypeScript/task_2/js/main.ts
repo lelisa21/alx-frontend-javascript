567cfe6b57d53a18fe00b9bb95925088c0bbf75b
Lelisa Hailu <189599041+lelisa21@users.noreply.github.com>
2025-09-13 12:31:45 +0300

    salary updated main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -33,7 +33,7 @@ class Teacher implements TeacherInterface {
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
-    if (typeof salary === 'number' && salary < 500) {
+    if (salary < 500) {
         return new Teacher();
     }
     return new Director();
